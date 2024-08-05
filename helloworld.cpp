6cf358824b41d9bfca9c8386e20b28ea1109dd18
Brendan Ly <brendanly4@gmail.com>
2024-08-05 16:03:08 -0700

    Introduction to C++

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+// typedef std::string text_t;
+using text_t = std::string;
+
+int main() {
+
+    text_t firstName = "Johnny";
+
+    std::cout << firstName;
+
+    return 0;
+} 
+
